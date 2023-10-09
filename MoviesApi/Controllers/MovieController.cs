8505d6b6bc6323de0081a073882b06deb8185e17
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-10-09 09:32:46 -0300

    Iniciando estrutura do projeto

---
+++ MoviesApi/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesApi.Models;
+
+namespace MoviesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> movies = new();
+
+    [HttpPost]
+    public void AddMovie([FromBody] Movie movie)
+    {
+        movies.Add(movie);
+        Console.WriteLine(movie.Title);
+        Console.WriteLine(movie.Duration);
+    }
+}
